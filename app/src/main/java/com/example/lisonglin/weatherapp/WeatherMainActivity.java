7b62b3b1bb15fbe239574f46b9ff46498f8933de
lisonglin <simugongzi@sina.com>
2014-09-15 19:53:42 +0800

    re layout

--- app/src/main/java/com/example/lisonglin/weatherapp/WeatherMainActivity.java
+++ app/src/main/java/com/example/lisonglin/weatherapp/WeatherMainActivity.java
@@ -5,19 +5,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 
 
 public class WeatherMainActivity extends Activity {
+    private FrameLayout ActivityMainBG;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_main);
 
-		
+        changeActivityMainBG();
+
+
     }
 
-
-
+public void changeActivityMainBG(){
+    ActivityMainBG =(FrameLayout)findViewById(R.id.MainBg);
+    ActivityMainBG.setBackground(this.getResources().getDrawable(R.drawable.weather_cloudy_night));
+}
 
 }
